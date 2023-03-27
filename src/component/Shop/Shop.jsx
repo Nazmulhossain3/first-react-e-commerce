a033e26b2238e96411a5b1291820786858914721
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-03-27 09:06:06 +0600

    Shop done

---
+++ src/component/Shop/Shop.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+
+    const [products,setProducts] = useState([])
+
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=> res.json())
+        .then(data => setProducts(data))
+
+
+    },[])
+
+    return (
+        <div className='shop-container'>
+
+            {/* this is Shop container */}
+
+            <div className="products-Container">
+            {
+                products.map(product => <Product
+                key={product.id}
+                product={product}>
+
+                </Product>)
+            }
+            </div>
+
+           
+           
+           {/* this is cart container */}
+           
+            <div className="cart-Container">
+                <h4>Please Order submit</h4>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
