e561234a85f39c44bfdd235e15cb9b544be9cf18
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-04-01 19:32:20 +0600

    save cart done

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -7,9 +7,12 @@ const Cart = (props) => {
 
     let total = 0;
     let totalShipping= 0;
+    let quantity = 0;
     for(const product of cart){
-         total = total + product.price
+        product.quantity = product.quantity || 1;
+         total = total + product.price * product.quantity
          totalShipping = totalShipping + product.shipping
+         quantity = quantity + product.quantity
     }
 
     const tax = total * 7 / 100;
@@ -18,7 +21,7 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h4>Order Summery</h4>
-            <p>Selected Item : {cart.length}</p>
+            <p>Selected Item : {quantity}</p>
             <p>Total Price : ${total}</p>
             <p>Total Shipping : {totalShipping}</p>
             <p>Tax : {tax}</p>
